0cb4e58811fef334ca52d16d5e6440544e126134
jbai@ust.hk <jbai@songcpu4.cse.ust.hk>
2018-09-24 03:35:26 +0800

    optimize

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -46,7 +46,7 @@ def svm_loss_naive(W, X, y, reg):
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
-  dW += - 2 * reg * W
+  dW +=  2 * reg * W
   #############################################################################
   # TODO:                                                                     #
   # Compute the gradient of the loss function and store it dW.                #
